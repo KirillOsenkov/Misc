1844e624dce64a7c1ac7a96ebdfda4d6ce2ad5f1
Kirill Osenkov <KirillOsenkov@users.noreply.github.com>
2018-07-14 14:39:47 -0700

    Create EditorRotator.cs

---
+++ EditorRotator.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ClassLibrary1
+{
+    public class EditorRotator
+    {
+        private static bool isEditorRotated = false;
+        public static bool IsEditorRotated
+        {
+            get
+            {
+                return isEditorRotated;
+            }
+
+            set
+            {
+                if (isEditorRotated == value)
+                {
+                    return;
+                }
+
+                isEditorRotated = value;
+                ScheduleWork();
+            }
+        }
+
+        public static void ScheduleWork()
+        {
+            Application.Current.Dispatcher.InvokeAsync(DoWorkCore);
+        }
+
+        private static void DoWorkCore()
+        {
+            var window = Application.Current.MainWindow;
+            var child = FindChildOfType(window, d => d.GetType().Name.EndsWith("WpfTextViewHost")) as FrameworkElement;
+            if (child == null)
+            {
+                return;
+            }
+
+            if (isEditorRotated)
+            {
+                child.RenderTransform = new RotateTransform(20.0, 500, 400);
+            }
+            else
+            {
+                child.RenderTransform = null;
+            }
+        }
+
+        private static object FindChildOfType(DependencyObject root, Func<DependencyObject, bool> predicate)
+        {
+            if (predicate(root))
+            {
+                return root;
+            }
+
+            foreach (var child in GetChildren(root))
+            {
+                var found = FindChildOfType(child, predicate);
+                if (found != null)
+                {
+                    return found;
+                }
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<DependencyObject> GetChildren(DependencyObject visual)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(visual);
+            var list = new List<DependencyObject>();
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(visual, i);
+                list.Add(child);
+            }
+
+            return list;
+        }
+    }
+}
