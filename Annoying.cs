844dea2f654f2e3f6c4552a487da592680c5147f
Kirill Osenkov <github@osenkov.com>
2023-11-25 11:35:25 -0800

    Add a solver for an annoying problem

---
+++ Annoying.cs
@@ -0,0 +1,68 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Solve(numbers: 9, pluses: 3, minuses: 1, target: 200, stringSoFar: "");
+    }
+
+    static void Solve(int numbers, int pluses, int minuses, int target, string stringSoFar = "")
+    {
+        if (numbers <= pluses + minuses)
+        {
+            return;
+        }
+
+        int currentNumber = 0;
+
+        for (int i = numbers; i > pluses + minuses; i--)
+        {
+            currentNumber = currentNumber * 10 + i;
+
+            if (pluses > 0)
+            {
+                string plusString = stringSoFar;
+
+                if (plusString != "")
+                {
+                    plusString = plusString + " + ";
+                }
+
+                plusString = $"{plusString}{currentNumber}";
+
+                Solve(i - 1, pluses - 1, minuses, target - currentNumber, $"{plusString}");
+            }
+
+            if (minuses > 0)
+            {
+                string minusString = stringSoFar;
+
+                if (minusString != "")
+                {
+                    minusString = minusString + " - ";
+                }
+                else
+                {
+                    minusString = "-";
+                }
+
+                minusString = $"{minusString}{currentNumber}";
+
+                Solve(i - 1, pluses, minuses - 1, target + currentNumber, $"{minusString}");
+            }
+        }
+
+        if (currentNumber == target)
+        {
+            stringSoFar = $"{stringSoFar} + {currentNumber}";
+
+            FoundSolution($"{stringSoFar}");
+        }
+    }
+
+    private static void FoundSolution(string solution)
+    {
+        Console.WriteLine(solution);
+    }
+}
