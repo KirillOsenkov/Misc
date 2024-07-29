a6345c508500e32d7f37bf88a23e1522e40bfc99
Kirill Osenkov <github@osenkov.com>
2024-07-29 09:52:28 -0700

    Add CredentialManagement.cs

---
+++ CredentialManagement.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+public static class CredentialManager
+{
+    public static byte[] GetCredentialBytes(string name)
+    {
+        if (!CredRead(name, CredentialType.Generic, 0, out IntPtr credentialPtr))
+        {
+            return null;
+        }
+
+        using (var handle = new CriticalCredentialHandle(credentialPtr))
+        {
+            var credential = handle.GetCredential();
+            if (credential.CredentialBlobSize > 0)
+            {
+                byte[] array = new byte[credential.CredentialBlobSize];
+                Marshal.Copy(credential.CredentialBlob, array, 0, array.Length);
+                return array;
+            }
+        }
+
+        return null;
+    }
+
+    public static string GetCredentialUnicodeString(string name)
+    {
+        var bytes = GetCredentialBytes(name);
+        var text = bytes.GetUnicodeString();
+        return text;
+    }
+
+    public static void SetCredentialUnicodeString(string name, string value)
+    {
+        var bytes = Encoding.Unicode.GetBytes(value);
+        SetCredentialBytes(name, bytes);
+    }
+
+    public static void SetCredentialBytes(string name, byte[] byteArray)
+    {
+        // XP and Vista: 512;
+        // 7 and above: 5*512
+        if (Environment.OSVersion.Version < new Version(6, 1) /* Windows 7 */)
+        {
+            if (byteArray != null && byteArray.Length > 512)
+            {
+                throw new ArgumentOutOfRangeException("secret", "The secret message has exceeded 512 bytes.");
+            }
+        }
+        else
+        {
+            if (byteArray != null && byteArray.Length > 512 * 5)
+            {
+                throw new ArgumentOutOfRangeException("secret", "The secret message has exceeded 2560 bytes.");
+            }
+        }
+
+        CREDENTIAL credential = new CREDENTIAL();
+        credential.AttributeCount = 0;
+        credential.Attributes = IntPtr.Zero;
+        credential.Comment = null;
+        credential.TargetAlias = null;
+        credential.Type = (int)CredentialType.Generic;
+        credential.Persist = 2; // CredentialPersistence.LocalMachine;
+        credential.CredentialBlobSize = byteArray == null ? 0 : byteArray.Length;
+        credential.TargetName = name;
+        credential.CredentialBlob = byteArray.WriteToNativeMemory();
+
+        credential.UserName = Environment.UserName;
+
+        bool written = CredWrite(ref credential, 0);
+        Marshal.FreeHGlobal(credential.CredentialBlob);
+
+        if (!written)
+        {
+            int lastError = Marshal.GetLastWin32Error();
+            throw new Exception(string.Format("CredWrite failed with the error code {0}.", lastError));
+        }
+    }
+
+    [DllImport("Advapi32.dll", CharSet = CharSet.Unicode, EntryPoint = "CredReadW", SetLastError = true)]
+    internal static extern bool CredRead(string target, CredentialType type, int reservedFlag, out IntPtr CredentialPtr);
+
+    [DllImport("Advapi32.dll", CharSet = CharSet.Unicode, EntryPoint = "CredWriteW", SetLastError = true)]
+    internal static extern bool CredWrite([In] ref CREDENTIAL userCredential, [In] UInt32 flags);
+
+    [DllImport("Advapi32.dll", SetLastError = true)]
+    internal static extern bool CredFree([In] IntPtr cred);
+
+    public static IntPtr WriteToNativeMemory(this byte[] array)
+    {
+        var pointer = Marshal.AllocHGlobal(array.Length);
+        Marshal.Copy(array, 0, pointer, array.Length);
+        return pointer;
+    }
+
+    public static string GetUnicodeString(this byte[] array)
+    {
+        if (array == null)
+        {
+            return null;
+        }
+
+        return Encoding.Unicode.GetString(array);
+    }
+
+    internal enum CredentialType : uint
+    {
+        None,
+        Generic,
+        DomainPassword,
+        DomainCertificate,
+        DomainVisiblePassword
+    }
+
+    internal sealed class CriticalCredentialHandle : CriticalHandleZeroOrMinusOneIsInvalid
+    {
+        internal CriticalCredentialHandle(IntPtr preexistingHandle)
+        {
+            SetHandle(preexistingHandle);
+        }
+
+        internal CREDENTIAL GetCredential()
+        {
+            if (!IsInvalid)
+            {
+                return (CREDENTIAL)Marshal.PtrToStructure(handle, typeof(CREDENTIAL));
+            }
+
+            throw new InvalidOperationException("Invalid CriticalHandle!");
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            if (!IsInvalid)
+            {
+                CredFree(handle);
+                SetHandleAsInvalid();
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    internal struct CREDENTIAL
+    {
+        public int Flags;
+
+        public int Type;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string TargetName;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string Comment;
+
+        public long LastWritten;
+
+        public int CredentialBlobSize;
+
+        public IntPtr CredentialBlob;
+
+        public int Persist;
+
+        public int AttributeCount;
+
+        public IntPtr Attributes;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string TargetAlias;
+
+        [MarshalAs(UnmanagedType.LPWStr)]
+        public string UserName;
+    }
+}
